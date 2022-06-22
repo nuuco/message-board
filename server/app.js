4b49115c8252c65a581451f8252e2a850300e459
nuuco <nuuco.coding@gmail.com>
2022-06-22 23:40:51 +0900

    feat: 리스트 라우터 경로 설정

--- server/app.js
+++ server/app.js
@@ -9,6 +9,14 @@ const PORT = 3333;
 // \app\anans\a12  <- 맥 경로
 // 이걸 맞춰주는게 path
 
+const data = [
+    { title: "양희준", content: "코드스테이츠1"},
+    { title: "심소영", content: "코드스테이츠2"},
+    { title: "이유진", content: "코드스테이츠3"}
+];
+
+
+
 app.use(express.json());
 app.use(cors());
 app.use(logger('tiny'));
@@ -18,10 +26,10 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname + '../client/build/i
 //path.join() 경로를 합쳐주는 메소드
 // __dirname : 현재 파일이 속한 폴더의 경로
 
+app.get("/list", (req, res) => res.status(200).json(data))
 
-// ---------- //
-
-
-
-app.listen(PORT, () => console.log('서버 실행중입니다.'))
+app.listen(PORT, () => {
+    console.log('서버 실행중입니다.');
+    //mongose.connect().then();
+})
 
